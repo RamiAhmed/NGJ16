0bd2dd61f5f551ffa7f920b968c95c49024d2f96
Mikkel Anthoni <mikkelanthoni200@gmail.com>
2016-04-09 17:44:12 +0200

    UI Changes
    UI changes

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+
+	public void OnClickStart(){
+		SceneManager.LoadScene("3DScene_Test1");
+
+	}
+
+	public void OnClickExit(){
+		Application.Quit();
+	}
+}
