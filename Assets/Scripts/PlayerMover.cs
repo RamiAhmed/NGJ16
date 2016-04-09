5a0a688a966a3efaf07646d0fc34aaae2f28ecd8
Rami Ahmed Bock <Rami@ApexGameTools.com>
2016-04-09 14:22:25 +0200

    Implemented rotational movement & fixed controller support for positional movement

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,10 +4,14 @@
 
     public class PlayerMover : MonoBehaviour
     {
-        [SerializeField]
+        [SerializeField, Range(1f, 1000f)]
         private float _acceleration = 400f;
 
+        [Range(1f, 200f)]
         public float friction = 4f;
+
+        [Range(1f, 90f)]
+        public float angularSpeed = 15f;
 
 #if UNITY_EDITOR
 
@@ -18,6 +22,9 @@
         public float timeToMaxSpeed;
 
 #endif
+
+        private float _horizontalRot;
+        private float _verticalRot;
 
         public Vector3 input
         {
@@ -67,6 +74,19 @@
             var speed = this.position + ((prevVelocity + this.velocity) / 0.5f) * deltaTime;
             speed.y = 0f;
             this.position = speed;
+
+            if (_horizontalRot != 0f || _verticalRot != 0f)
+            {
+                var angle = Mathf.Atan2(_verticalRot, _horizontalRot) * Mathf.Rad2Deg;
+                var newAngle = Quaternion.AngleAxis(angle + 90f, Vector3.up);
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, newAngle, Time.deltaTime * this.angularSpeed);
+            }
+        }
+
+        public void Rotate(float horizontal, float vertical)
+        {
+            _horizontalRot = horizontal;
+            _verticalRot = vertical;
         }
     }
 }
