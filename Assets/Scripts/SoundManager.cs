6a640b786c0023087a277e980d4218fb702d0987
Kevin Broløs <kevin.broloes@bestbrains.dk>
2016-04-09 18:32:05 +0200

    Add preliminary sound manager/player

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+	
+	public AudioClip[] BackgroundMusic;
+	public AudioClip PlayerHit;
+	public AudioClip PlayerDash;
+	public AudioSource backgroundPlayer;
+
+	private AudioClip nextTrack;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		QueueNextTrack();
+		PlayTrack();
+	}
+
+	void PlayTrack() {
+		if(!backgroundPlayer.isPlaying) {
+			backgroundPlayer.clip = nextTrack;
+			backgroundPlayer.Play();
+			nextTrack = null;
+		}
+	}
+
+	AudioClip GetTrack() {
+		//TODO: Crazy logic here.
+		if(BackgroundMusic.Length > 0) {
+			
+			int idx = Random.Range(0, BackgroundMusic.Length);
+			print("Playing " + idx);
+			return BackgroundMusic[idx];
+		}
+		return null;
+	}
+
+	void QueueNextTrack() {
+		if(nextTrack == null) {
+			nextTrack = GetTrack();
+		}
+	}
+}
